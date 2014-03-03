cd367a3a5f25d4ce49f1bb54697b0cf925f29468
Teddy Zeenny <teddyzeenny@gmail.com>
2014-03-03 11:49:53 +0200

    Don't check if defined

--- lib/ember/appkit/rails/teaspoon/spec_controller.rb
+++ lib/ember/appkit/rails/teaspoon/spec_controller.rb
@@ -9,5 +9,8 @@ module Ember::Appkit::Rails::Teaspoon::SpecController
 end
 
 ActiveSupport.on_load(:action_controller) do
-  Teaspoon::SpecController.send(:prepend, Ember::Appkit::Rails::Teaspoon::SpecController) if defined?(Teaspoon::SpecController)
+
+  # if defined?(Teaspoon::SpecController)
+    Teaspoon::SpecController.send(:prepend, Ember::Appkit::Rails::Teaspoon::SpecController)
+  # end
 end
